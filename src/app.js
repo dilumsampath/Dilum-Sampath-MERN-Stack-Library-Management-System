724eb080c9bb83336ffa6e5bf6a9e4254b2be127
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-06-20 04:41:43 +0530

    create app.js file as the backend starting point

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 //Due to using ES7, we need to import the modules ; if we use older ES then we should use require("express") likewise
 
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h2>📚 Library Management System API</h2>");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
